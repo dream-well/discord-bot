83c192269d9555e477139c0737a635c34e9115a9
dream-well <dreamwell506@outlook.com>
2023-03-20 01:13:41 +0900

    move application id

--- index.ts
+++ index.ts
@@ -31,7 +31,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
   try {
     console.log('Started refreshing application (/) commands.');
 
-    await rest.put(Routes.applicationCommands("1085831488458739712"), { body: commands });
+    await rest.put(Routes.applicationCommands(process.env.APPLICATION_ID), { body: commands });
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
     console.error(error);
